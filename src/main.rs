03c135a16f00f9e8a763c5181deee298e704f816
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-09-28 00:11:24 +0530

    Main rs -> new instance fro kvstore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+# Import the kv_store module
+mod kv_store;
+
 fn main() {
-    println!("Hello, world!");
+    let mut store = kv.store::KvStore::new();
+    store.set("key1".to_string(), "value1".to_string());
+    if let Some(value) = store.get("key1"){
+        println!("key1: {}", value);
+    }
 }
