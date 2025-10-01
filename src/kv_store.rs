6d32bbba17ece5f2bb67e77e89abdc2c5888f1e4
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-10-01 15:47:35 +0530

    Implement hashmap on kvstore

---
+++ src/kv_store.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+// Struct for kvstore
+
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {
+            store: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.store.get(key)
+    }
+}
