60f2cb55194e4a9b266a5ada94fe6e76b31f45d1
shubhamsugara22 <shubhamsugara22@gmail.com>
2025-10-30 01:01:23 +0530

    Add server.rs to store

---
+++ src/server.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpServer};
+use rust_kv_store::{api, kv_store::KvStore};
+use std::sync::Mutex;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let bind = env::var("KV_BIND").unwrap_or_else(|_| "127.0.0.1:8080".into());
+    let store = web::Data::new(Mutex::new(KvStore::new()));
+
+    println!("Starting server at http://{}", bind);
+
+    HttpServer::new(move || {
+        App::new().app_data(store.clone()).service(
+            web::scope("/api")
+                .route("/keys", web::get().to(api::list_keys))
+                .route("/keys/{key}", web::get().to(api::get_value))
+                .route("/keys/{key}", web::put().to(api::set_value))
+                .route("/keys/{key}", web::delete().to(api::delete_value)),
+        )
+    })
+    .bind(bind)?
+    .run()
+    .await
+}
